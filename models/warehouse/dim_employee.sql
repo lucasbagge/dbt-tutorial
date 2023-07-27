3376fbc0fb1aaee897380420508bf18e2de716c1
lucas <lucasbagge@hotmail.com>
2023-07-27 23:15:51 +0200

    added final

---
+++ models/warehouse/dim_employee.sql
@@ -0,0 +1,38 @@
+WITH source AS (
+
+SELECT
+    id AS employee_id
+    , company
+    , last_name
+    , first_name
+    , email_address
+    , job_title
+    , business_phone
+    , home_phone
+    , mobile_phone
+    , fax_number
+    , address
+    , city
+    , state_province
+    , zip_postal_code
+    , country_region
+    , web_page
+    , notes
+    , attachments
+    , current_timestamp() AS insertion_timestamp
+from {{ ref('stg_employee') }}
+)
+
+, unique_source AS (
+    select
+        *
+        , ROW_NUMBER() over(partition by employee_id) as row_number
+    from source
+)
+
+select
+    *
+    except (row_number)
+from unique_source
+where 
+    row_number = 1
